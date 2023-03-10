4c1e43dec7929e7a37b2238cada528ff0a17db59
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-03-10 17:44:23 -0600

    added javascript for full functionality

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+$(document).ready(function() {
+  const timeBlocksContainer = $('.container-lg');
+  const dateContainer = $('#currentDay');
+
+  // Create and append time blocks for each hour of the day
+  for (let i = 0; i <= 23; i++) {
+    const timeBlock = $('<div>').attr('id', 'hour-' + i).addClass('row time-block');
+
+    // Add appropriate class for past, present, or future time
+    if (i < dayjs().hour()) {
+      timeBlock.addClass('past');
+    } else if (i === dayjs().hour()) {
+      timeBlock.addClass('present');
+    } else {
+      timeBlock.addClass('future');
+    }
+
+    // Add hour text
+    const hour = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(dayjs().hour(i).format('hA'));
+    timeBlock.append(hour);
+
+    // Add textarea
+    const description = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', '3');
+
+    // Get saved text from local storage, if any
+    const savedText = localStorage.getItem('hour-' + i);
+    if (savedText) {
+      description.val(savedText);
+    }
+
+    timeBlock.append(description);
+
+    // Add save button
+    const button = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
+
+    // Save text to local storage when button is clicked
+    button.on('click', function() {
+      const text = description.val();
+      localStorage.setItem('hour-' + i, text);
+    });
+
+    const icon = $('<i>').addClass('fas fa-save').attr('aria-hidden', true);
+    button.append(icon);
+    timeBlock.append(button);
+
+    timeBlocksContainer.append(timeBlock);
+  }
+
+  // Update date/time every second
+  let updateDateTime = function() {
+    const now = dayjs();
+    const timeString = now.format('h:mm:ss A');
+    const dateString = now.format('MMM DD, YYYY');
+    dateContainer.text(dateString + ' ' + timeString);
+  }
+  setInterval(updateDateTime, 1000);
 });
